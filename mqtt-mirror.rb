28006b0ab5d114b75f1d0a8e3b185444a7bf54ad
goreleaserbot <gnante0@gmail.com>
2020-02-02 19:02:24 +0100

    Brew formula update for mqtt-mirror version v0.1.29

--- mqtt-mirror.rb
+++ mqtt-mirror.rb
@@ -2,16 +2,16 @@
 class MqttMirror < Formula
   desc "Replicate MQTT traffic from one broker to another."
   homepage "https://github.com/4nte/mqtt-mirror"
-  version "0.1.28"
+  version "0.1.29"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/4nte/mqtt-mirror/releases/download/v0.1.28/mqtt-mirror_0.1.28_Darwin_x86_64.tar.gz"
-    sha256 "1b89a048aa43087c62a80e71f7c25970d6e4a65e3ce6d61a325a5c0d59942f14"
+    url "https://github.com/4nte/mqtt-mirror/releases/download/v0.1.29/mqtt-mirror_0.1.29_Darwin_x86_64.tar.gz"
+    sha256 "5c3afa67d0701e938f1b00ee112a7e37fdd6efc1a4971f01ca31a05dfb146fc7"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/4nte/mqtt-mirror/releases/download/v0.1.28/mqtt-mirror_0.1.28_Linux_x86_64.tar.gz"
-      sha256 "b1b722a810eade169b6aabea9528275496502d9b0f6a331d18be278dd5be85f3"
+      url "https://github.com/4nte/mqtt-mirror/releases/download/v0.1.29/mqtt-mirror_0.1.29_Linux_x86_64.tar.gz"
+      sha256 "38e1652c4ddbf0cbc4948866a5a6b77e5fa700027b47741de28e50e95a1f7c94"
     end
   end
 
